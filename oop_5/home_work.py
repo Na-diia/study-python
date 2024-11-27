80ffd65a7928d12774a583721ca14509657dc329
Pekaruk Nadiia <108739131+Na-diia@users.noreply.github.com>
2024-11-27 22:29:51 +0200

    update home_work.py

--- oop_5/home_work.py
+++ oop_5/home_work.py
@@ -1,23 +1,42 @@
-# **Завдання 1: Створення класу і об'єктів**
-#
-# Створіть клас `Animal`, який представляє тварину. Кожний об'єкт класу `Animal` повинен мати наступні атрибути:
-#
-# - `name` (ім'я тварини)
-# - `species` (вид тварини)
-# - `age` (вік тварини)
-#
-# Створіть конструктор класу, який ініціалізує ці атрибути при створенні об'єкта. Напишіть метод `make_sound()`, який буде виводити звук, який виділяє тварина.
-#
-# Створіть два об'єкта класу `Animal` з різними характеристиками та викличте їхні методи `make_sound()`.
-#
+
+class Animal:
+    """Class to create an animal"""
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age
+
+    def make_sound(self):
+        if self.species == 'Cat':
+                print('Meow')
+        elif self.species == 'Dog':
+                print('Bark')
+        elif self.species == 'Bird':
+                print("Beautiful song")
+        elif self.species == 'Fish':
+                print('Bull')
+        else:
+                print("we don't know this animal")
+
+animal1 = Animal('Balto', 'Dog', 5)
+animal2 = Animal('Kotyhoroshko', 'Cat', 4)
+animal1.make_sound()
+animal2.make_sound()
+
 # **Завдання 2: Робота з об'єктами**
-#
-# Створіть клас `Rectangle`, який представляє прямокутник. Кожен об'єкт класу `Rectangle` повинен мати наступні атрибути:
-#
-# - `width` (ширина прямокутника)
-# - `height` (висота прямокутника)
-#
-# Створіть конструктор класу, який ініціалізує ці атрибути при створенні об'єкта. Напишіть метод `calculate_area()`, який розраховує площу прямокутника (площа = ширина * висота).
-#
-# Створіть два об'єкта класу `Rectangle` з різними розмірами та викличте їхні методи `calculate_area()`, виведіть площу прямокутників на екран.
+
+class Restangle:
+    """Class to present restangle"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        area = self.height * self.width
+        print(area)
+
+item1 = Restangle(10, 30)
+item2= Restangle(5, 10)
+item1.calculate_area()
+item2.calculate_area()
 
