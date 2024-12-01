c756644c55103980270100af640a461256384cce
Pekaruk Nadiia <108739131+Na-diia@users.noreply.github.com>
2024-12-01 16:57:32 +0200

    update home_work.py

--- lesson_7/home_work.py
+++ lesson_7/home_work.py
@@ -1,21 +1,56 @@
 # Завдання 1: Інкапсуляція
-#
-# Створіть клас "Користувач" (User), який має такі приватні поля (інкапсульовані дані):
-#
-# Ім'я (name)
-# Електронна пошта (email)
-# Пароль (password)
-# Напишіть публічні методи для установки і отримання значень цих полів (геттери і сеттери). Потім створіть об'єкт класу "Користувач" і встановіть значення полів, а також виведіть їх на екран.
-#
+class User:
+    """Class for creating users"""
+
+    def __init__(self, name, email, password):
+        self.__name = name
+        self.__email = email
+        self.__password = password
+
+    def get_data(self):
+        return  self.__dict__
+
+    def set_data(self, attr, value):
+        if type(value) == str:
+            self.__dict__[attr] = value
+            return {attr: self.__dict__[attr]}
+        else:
+            print("This type isn't string")
+
+user_1 = User("Dmytro", "dmytro@gmail.com", "potter")
+user_1.__dict__
+print(user_1.get_data())
+user_1.set_data("_User__name", "Vira")
+print(user_1.get_data())
 # Завдання 2: Абстракція
-#
-# Створіть клас "Фігура" (Shape), який буде абстрактним класом. У цьому класі визначіть абстрактний метод "обчислити_площу" (calculate_area).
-#
-# Створіть підкласи цього класу для різних геометричних фігур, наприклад, "Коло" (Circle), "Прямокутник" (Rectangle) і "Трикутник" (Triangle). У кожному з підкласів реалізуйте метод "обчислити_площу" відповідно до формули для обчислення площі кожної фігури.
-#
-# Створіть об'єкти кожного з підкласів і використайте метод "обчислити_площу", щоб вивести площу кожної фігури на екран.
-#
-# Завдання 3: Користування інкапсуляцією та абстракцією у реальному коді
-#
-# Розгляньте фрагмент коду з існуючого проекту або бібліотеки та ідентифікуйте в ньому використання інкапсуляції та абстракції. Поясніть, як вони застосовуються і як це допомагає поліпшити читабельність та підтримку коду.
+from abc import ABC, abstractclassmethod
+
+class Shape(ABC):
+
+    @abstractclassmethod
+    def calculate_area(self):
+        pass
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        print(f"Area of this circle is {3.14 * self.radius * 2}")
+
+class Rectangle(Shape):
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def calculate_area(self):
+        print(f"The area of this rectangle is {self.a * self.b}")
+
+circle = Circle(6)
+circle.calculate_area()
+
+rectangle = Rectangle(10, 5)
+rectangle.calculate_area()
 
